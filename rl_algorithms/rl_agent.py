942db932e989efe48f4c084eacde7207bd6e9670
Jessica <jml19@ic.ac.uk>
2023-02-13 17:04:53 +0000

    Restructure Q-Learning to have generalised RL agent wrapper

---
+++ rl_algorithms/rl_agent.py
@@ -0,0 +1,12 @@
+class RLAgent:
+    def __init__(self, env):
+        pass
+
+    def train(self):
+        """ Trains the RL agent. """
+        pass
+
+    def generate_test_data(self):
+        """ Generates example (state, action, next_state) tuples from the 
+        trained agent to be used in evaluation. """ 
+        pass
