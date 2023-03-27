28a1543ec220ebe7c37b28cab3ecb4d4429161f6
Jessica <jml19@ic.ac.uk>
2023-03-27 15:40:56 +0100

    Generate training data for causal discovery

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from environments.cartpole import Cartpole
 import evaluation
 from rl_algorithms.q_learning import QLearning
 import gym
+import utils
 
 # Choose which environment to use
 environment = Cartpole()
@@ -20,10 +21,18 @@ action_influence_model = ActionInfluenceModel(
 action_influence_model.train()
 
 # Evaluation
-test_data = q_learning.generate_test_data(environment.env, q_table)
-accuracy = evaluation.task_prediction(test_data, action_influence_model)
-print("Accuracy="+str(accuracy))
-fidelity = evaluation.evaluate_fidelity(test_data, action_influence_model)
-print("Fidelity="+str(fidelity))
+# test_data = q_learning.generate_test_data(environment.env, q_table)
+# accuracy = evaluation.task_prediction(test_data, action_influence_model)
+# print("Accuracy="+str(accuracy))
+# fidelity = evaluation.evaluate_fidelity(test_data, action_influence_model)
+# print("Fidelity="+str(fidelity))
+# faithfulness = evaluation.evaluate_faithfulness(
+#     test_data, 
+#     action_influence_model
+# )
 
 # TODO: processing explanations
+
+# Causal graph discovery
+data = q_learning.generate_data_for_causal_discovery(environment.env, q_table)
+utils.convert_data_to_csv(data, "X.csv")
