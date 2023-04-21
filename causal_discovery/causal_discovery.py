67d85559d534d112041b4219110efa8912444bc5
Jessica <jml19@ic.ac.uk>
2023-04-21 18:13:03 +0100

    Implement causal discovery for various methods and environments

---
+++ causal_discovery/causal_discovery.py
@@ -0,0 +1,95 @@
+import argparse
+from castle.common import GraphDAG
+from castle.metrics import MetricsDAG
+from environment import Environment
+from method import Method
+import numpy as np
+import os
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Causal Discovery')
+    parser.add_argument(
+        'path_to_dataset',
+        type=str,
+        help='Path to CSV file containing training data')
+    parser.add_argument(
+        '--env',
+        type=str,
+        default='cartpole',
+        help='RL environment for performing causal discovery on')
+    parser.add_argument('--method', type=str, default='varlingam',
+                        help='Causal discovery method')
+    parser.add_argument('--save_path', type=str, default='output/',
+                        help='Path to save generated causal graphs')
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+
+    return args
+
+
+def get_data(path, num_datapoints=30000):
+    if not os.path.exists(path):
+        raise argparse.ArgumentError(f'file {args.dataset} does not exist')
+
+    data = open(path)
+    data = np.loadtxt(data, delimiter=",")[:num_datapoints, :]
+    print(f'Data: {data.shape}')
+
+    return data
+
+
+def display_graphs(causal_matrix, true_dag, args, with_assumptions):
+    save_path = f'{args.save_path}_{args.env}_{args.method}_assumptions_{with_assumptions}'
+    GraphDAG(causal_matrix, true_dag, show=True, save_name=save_path)
+
+
+def evaluate(causal_matrix, true_dag):
+    met = MetricsDAG(causal_matrix, true_dag)
+    print(met.metrics)
+
+
+def main(args):
+    # TODO: VarLiNGAM currently requires a different CSV structure than the
+    # other algorithms (VarLiNGAM handles data in rows of state + action, but
+    # the other algorithms need state + action + next state)
+    data = get_data(args.path_to_dataset, num_datapoints=30000)
+    env = Environment.get_env(args.env)
+    method = Method.get_method(args.method)
+
+    causal_matrix_no_assumptions = method.generate_causal_matrix(
+        data,
+        env,
+        with_assumptions=False
+    )
+
+    causal_matrix_with_assumptions = method.generate_causal_matrix(
+        data,
+        env,
+        with_assumptions=True
+    )
+
+    display_graphs(
+        causal_matrix_no_assumptions,
+        env.true_dag,
+        args,
+        False
+    )
+
+    display_graphs(
+        causal_matrix_with_assumptions,
+        env.true_dag,
+        args,
+        True
+    )
+
+    evaluate(causal_matrix_no_assumptions, env.true_dag)
+    evaluate(causal_matrix_with_assumptions, env.true_dag)
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
---
+++ causal_discovery/environment.py
@@ -0,0 +1,247 @@
+import numpy as np
+
+
+class Environment(object):
+    true_dag = None
+    # TODO: currently needed for VarLiNGAM DAG plots, find a better way to
+    # generalise for all methods
+    labels = None
+    forbidden_edges = []
+    required_edges = []
+
+    @staticmethod
+    def get_env(env):
+        if env == 'cartpole':
+            return Cartpole()
+        elif env == 'lunarlander':
+            return LunarLander()
+        else:
+            raise ValueError(f'{env} is an unsupported environment')
+
+##### Cartpole #####
+
+
+class Cartpole(Environment):
+    true_dag = np.array([
+        [0, 0, 0, 0, 1, 1, 0, 0, 0],  # 0 = pos t
+        [0, 0, 0, 0, 1, 1, 1, 0, 0],  # 1 = velocity t
+        [0, 0, 0, 0, 1, 0, 0, 1, 0],  # 2 = pole angle t
+        [0, 0, 0, 0, 1, 0, 0, 1, 1],  # 3 = angular velocity t
+        [0, 0, 0, 0, 0, 1, 1, 1, 1],  # 4 = action t
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],  # 5 = pos t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],  # 6 = velocity t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],  # 7 = pole angle t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]  # 8 = angular t+1
+    ])
+
+    labels = [
+        'pos(t)',
+        'velo(t)',
+        'angle(t)',
+        'ang-velo(t)',
+        'action(t)',
+        'pos(t-1)',
+        'velo(t-1)',
+        'angle(t-1)',
+        'ang-velo(t-1)',
+        'action(t-1)']
+
+    # Assumption: we cannot have any causal relationships that go backwards in
+    # time
+    forbidden_edges = [
+        (4, 0),
+        (4, 1),
+        (4, 2),
+        (4, 3),
+        (5, 0),
+        (5, 1),
+        (5, 2),
+        (5, 3),
+        (5, 4),
+        (6, 0),
+        (6, 1),
+        (6, 2),
+        (6, 3),
+        (6, 4),
+        (7, 0),
+        (7, 1),
+        (7, 2),
+        (7, 3),
+        (7, 4),
+        (8, 0),
+        (8, 1),
+        (8, 2),
+        (8, 3),
+        (8, 4),
+    ]
+
+    # Assumption: all past state variables affect action choice and action
+    # affects all future state variables
+    required_edges = [
+        (0, 4),
+        (1, 4),
+        (2, 4),
+        (3, 4),
+        (4, 5),
+        (4, 6),
+        (4, 7),
+        (4, 8),
+    ]
+
+
+##### Lunar Lander #####
+
+class LunarLander(Environment):
+    true_dag = np.array([
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # 0 = x coord t
+        [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0],  # 1 = y coord t
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0],  # 2 = x velocity t
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0],  # 3 = y velocity t
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],  # 4 = angle t
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+            1, 1, 0, 0],  # 5 = angular velocity t
+        # 6 = left leg in contact with ground t
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        # 7 = right leg in contact with ground t
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],  # 8 = action t
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],  # 9 = x coord t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],  # 10 = y coord t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 11 = x velocity t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 12 = y velocity t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 13 = angle t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0],  # 14 = angular velocity t+1
+        # 15 = left leg in contact with ground t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        # 16 = right leg in contact with ground t+1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+
+    labels = [
+        'x-coord(t)',
+        'y-coord(t)',
+        'x-velo(t)',
+        'y-velo(t)',
+        'ang(t)',
+        'ang-velo(t)',
+        'left-leg(t)',
+        'right-leg(t)',
+        'action(t)',
+        'x-coord(t-1)',
+        'y-coord(t-1)',
+        'x-velo(t-1)',
+        'y-velo(t-1)',
+        'ang(t-1)',
+        'ang-velo(t-1)',
+        'left-leg(t-1)',
+        'right-leg(t-1)',
+        'action(t-1)']
+
+    # Assumption: we cannot have any causal relationships that go backwards in
+    # time
+    forbidden_edges = [
+        (9, 0),
+        (9, 1),
+        (9, 2),
+        (9, 3),
+        (9, 4),
+        (9, 5),
+        (9, 6),
+        (9, 7),
+        (9, 8),
+        (9, 9),
+        (10, 0),
+        (10, 1),
+        (10, 2),
+        (10, 3),
+        (10, 4),
+        (10, 5),
+        (10, 6),
+        (10, 7),
+        (10, 8),
+        (10, 9),
+        (11, 0),
+        (11, 1),
+        (11, 2),
+        (11, 3),
+        (11, 4),
+        (11, 5),
+        (11, 6),
+        (11, 7),
+        (11, 8),
+        (11, 9),
+        (12, 0),
+        (12, 1),
+        (12, 2),
+        (12, 3),
+        (12, 4),
+        (12, 5),
+        (12, 6),
+        (12, 7),
+        (12, 8),
+        (12, 9),
+        (13, 0),
+        (13, 1),
+        (13, 2),
+        (13, 3),
+        (13, 4),
+        (13, 5),
+        (13, 6),
+        (13, 7),
+        (13, 8),
+        (13, 9),
+        (13, 0),
+        (14, 0),
+        (14, 1),
+        (14, 2),
+        (14, 3),
+        (14, 4),
+        (14, 5),
+        (14, 6),
+        (14, 7),
+        (14, 8),
+        (14, 9),
+        (15, 0),
+        (15, 1),
+        (15, 2),
+        (15, 3),
+        (15, 4),
+        (15, 5),
+        (15, 6),
+        (15, 7),
+        (15, 8),
+        (15, 9),
+        (16, 0),
+        (16, 1),
+        (16, 2),
+        (16, 3),
+        (16, 4),
+        (16, 5),
+        (16, 6),
+        (16, 7),
+        (16, 8),
+        (16, 9)
+    ]
+
+    # Assumption: all past state variables affect action choice and action
+    # affects all future state variables
+    required_edges = [
+        (0, 8),
+        (1, 8),
+        (2, 8),
+        (3, 8),
+        (4, 8),
+        (5, 8),
+        (6, 8),
+        (7, 8),
+
+        (8, 9),
+        (8, 10),
+        (8, 11),
+        (8, 12),
+        (8, 13),
+        (8, 14),
+        (8, 15),
+        (8, 16),
+    ]
