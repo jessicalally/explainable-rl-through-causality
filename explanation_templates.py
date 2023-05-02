70d7be02fae9a3449b925eb81741c98b3ce18b59
Jessica <jml19@ic.ac.uk>
2023-05-02 20:57:27 +0100

    WIP: generating explanations from known and learnt causal graphs

--- explanation_templates.py
+++ explanation_templates.py
@@ -36,6 +36,26 @@ cartpole_features = {
     3: 'pole angular velocity'
 }
 
+taxi_actions = {
+    0: 'move south',
+    1: 'move north',
+    2: 'move east',
+    3: 'move west',
+    4: 'pickup passenger',
+    5: 'drop off passenger'
+}
+
+taxi_features = {
+    0: 'taxi row',
+    1: 'taxi column',
+    2: 'passenger location',
+    3: 'destination',
+    4: 'action',
+    5: 'taxi row',
+    6: 'taxi column',
+    7: 'passenger location',
+    8: 'destination',
+}
 
 def cartpole_generate_why_text_explanations(
         min_tuple_actual_state,
@@ -69,6 +89,54 @@ def cartpole_generate_why_text_explanations(
             for op_imm in min_tuple_optimal_state['head']:
                 exp_string += ' (optimal ' + str(op_imm[1]) + ') '
 
+    return exp_string
+
+def taxi_generate_why_text_explanations(min_tuple_noop_transition, min_tuple_optimal_transition, action):
+    # Need to pass the previous state into here as well
+    # TODO: replace increase with the diff between the reward states - whatecer values have changed
+    # TODO: actually check what elements of the causal chain are being used here, not sure they are right (might all be at time t)
+    exp_string = 'Do: ' + \
+        taxi_actions[action] + '\nBecause: goal is to increase/decrease'
+
+    for reward in min_tuple_noop_transition['reward']:
+        exp_string += ', ' + str(taxi_features[reward[0]])
+
+    if len(min_tuple_noop_transition['immediate']) > 1:
+        exp_string += ': Which is influenced by'
+
+        for immed in min_tuple_noop_transition['immediate']:
+            exp_string += ', ' + \
+                str(taxi_features[immed[0]]) + ' (current ' + str(immed[1]) + ')'
+            for op_imm in min_tuple_optimal_transition['immediate']:
+                exp_string += ' (optimal ' + str(op_imm[1]) + ') '
+
+    if len(min_tuple_noop_transition['head']) > 0:
+        exp_string += ': that depend on'
+
+        for immed in min_tuple_noop_transition['head']:
+            exp_string += ', ' + \
+                str(taxi_features[immed[0]]) + ' (current ' + str(immed[1]) + ')'
+            
+            for op_imm in min_tuple_optimal_transition['head']:
+                exp_string += ' (optimal ' + str(op_imm[1]) + ') '
+
+    return exp_string
+
+def taxi_generate_contrastive_text_explanations(minimal_tuple, action):
+    exp_string = 'Because it is more desirable to do action ' + \
+        str(taxi_actions[action]) + ', '
+
+    for key in minimal_tuple['actual'].keys():
+        if minimal_tuple['actual'][key] >= minimal_tuple['counterfactual'][key]:
+            exp_string += 'to have more ' + str(taxi_features[key]) + ' (actual ' + str(
+                minimal_tuple['actual'][key]) + ') (counterfactual ' + str(minimal_tuple['counterfactual'][key]) + '), '
+        if minimal_tuple['actual'][key] < minimal_tuple['counterfactual'][key]:
+            exp_string += 'to have less ' + str(taxi_features[key]) + ' (actual ' + str(
+                minimal_tuple['actual'][key]) + ') (counterfactual ' + str(minimal_tuple['counterfactual'][key]) + '), '
+    exp_string += 'as the goal is to have '
+
+    for key in minimal_tuple['reward'].keys():
+        exp_string += '' + str(taxi_features[key]) + ', '
     return exp_string
 
 
