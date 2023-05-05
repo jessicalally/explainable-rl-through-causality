52c428d4eaabf1614c3f38d24ec6a6f403fddfc4
Jessica <jml19@ic.ac.uk>
2023-05-05 11:36:28 +0100

    Train policy gradient RL agent for Cartpole environment

--- main.py
+++ main.py
@@ -2,18 +2,43 @@ from causal_discovery.environment import *
 from causal_discovery.method import *
 import causal_discovery
 import evaluation
+import os
 import numpy as np
+import dill as pickle
 from rl_algorithms.SARSA import SARSA
+from rl_algorithms.policy_gradient import PolicyGradient
 from structural_causal_model import StructuralCausalModel
 
 # Choose which environment to use
-env = Taxi()
+# env = Taxi()
+env = Cartpole()
 
 # Choose which RL algorithm to use
-rl_agent = SARSA(env)
+# Train agent from scratch
+# rl_agent = SARSA(env)
+rl_agent = PolicyGradient(env)
 action_influence_dataset, causal_discovery_dataset = rl_agent.train()
 print(causal_discovery_dataset.shape)
 print(causal_discovery_dataset)
+
+rl_agent_path = "trained_rl_agents/pg.pickle"
+# os.makedirs(os.path.dirname(rl_agent_path), exist_ok=True)
+
+dataset_path = "causal_discovery_dataset/cartpole_pg.pickle"
+# os.makedirs(os.path.dirname(dataset_path), exist_ok=True)
+
+# with open(rl_agent_path, 'wb') as agent_file:
+#     pickle.dump(rl_agent,agent_file)
+
+# with open(dataset_path, 'wb') as dataset_file:
+#     pickle.dump(causal_discovery_dataset, dataset_file)
+
+# OR load from file
+with open(rl_agent_path,'rb') as agent_file:
+    rl_agent = pickle.load(agent_file)
+
+with open(dataset_path, 'rb') as dataset_file:
+    causal_discovery_dataset = pickle.load(dataset_file)
 
 # Perform causal discovery to generate causal graph
 method = VarLiNGAM()
@@ -24,30 +49,24 @@ causal_matrix_with_assumptions = method.generate_causal_matrix(
     with_assumptions=True
 )
 
-learned_causal_graph = causal_graph = nx.from_numpy_matrix(
+learned_causal_graph = nx.from_numpy_matrix(
             causal_matrix_with_assumptions, create_using=nx.MultiDiGraph())
 
-# num_datapoints = 1000
+# Train structural causal model
 
-# test_data = rl_agent.generate_test_data(num_datapoints)
-# rnd_indices = np.random.choice(len(test_data), 1000)
-# test_data = test_data[rnd_indices]
-
-# TODO: test_data should be replaced by the action influence training data or randomly generated states and actions from the env
-# TODO: especially as it looks like it might be overfitting
 scm = StructuralCausalModel(
     env,
     causal_discovery_dataset,
-    learned_causal_graph
+    # learned_causal_graph
 )
 
 scm.train()
 
 # Evaluation
-num_datapoints = 1000
+num_datapoints = 10000
 
 test_data = rl_agent.generate_test_data(num_datapoints)
-rnd_indices = np.random.choice(len(test_data), 1000)
+rnd_indices = np.random.choice(len(test_data), 2500)
 test_data = test_data[rnd_indices]
 
 print(f'Data: {test_data.shape}')
@@ -59,11 +78,6 @@ mse, action_predictions = evaluation.evaluate_fidelity(scm, test_data)
 print(causal_matrix_with_assumptions)
 print("MSE=" + str(mse))
 print("Correct action predictions=" + str(action_predictions))
-
-# faithfulness = evaluation.evaluate_faithfulness(
-#     test_data,
-#     action_influence_model
-# )
 
 # Processing explanations
 # why_explanations, why_not_explanations = scm.process_explanations(test_data)
