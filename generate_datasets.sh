a96bce48a6890ff62a4f4c634533cabb0238de6c
Jessica <jml19@ic.ac.uk>
2023-05-15 21:29:24 +0100

    Generate datasets

---
+++ generate_datasets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OLDIFS="IFS"
+IFS=','
+for i in "cartpole","ddqn" "lunarlander","ddqn" "taxi","sarsa" "mountaincar","dqn"
+do
+    set -- $i
+    echo $1 and $2
+    python main.py --env $1 --rl $2
+done
