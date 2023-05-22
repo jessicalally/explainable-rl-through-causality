37b99ea338013417ad7fc3ed9331b72299ff6009
Jessica <jml19@ic.ac.uk>
2023-05-22 10:08:39 +0100

    Convert csv files to pickle

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import dill as pickle
 
 
 def convert_data_to_csv(data, filename):
@@ -12,3 +13,15 @@ def convert_csv_to_npy(filename, save_filename):
     
     with open(save_filename, 'wb') as f:
         np.save(f, data)
+
+
+def convert_csv_to_npy_pickle(filename, save_filename):
+    data = np.loadtxt(filename, delimiter=',')
+    
+    with open(save_filename, 'wb') as f:
+        pickle.dump(data, f)
+
+
+if __name__ == "__main__":
+    convert_csv_to_npy_pickle("starcraft_causal_discovery.csv", "causalstarcraft_a2c.pickle")
+    convert_csv_to_npy_pickle("starcraft_reward_causal_discovery.csv", "rewardstarcraft_a2c.pickle")
